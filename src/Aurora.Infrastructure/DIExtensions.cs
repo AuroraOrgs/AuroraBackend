c31c4527bd660260f005ebeb8f3b73f5cfaa6b8f
korov <korova2321@gmail.com>
2021-09-02 01:16:57 +0300

    Created infrastructure project

---
+++ src/Aurora.Infrastructure/DIExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Aurora.Infrastructure
+{
+    public static class DIExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            //register all of the services
+            return services;
+        }
+    }
+}
