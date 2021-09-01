3cdc9ec76a049c48c5f541baedd7a4dbfbf253fe
korov <korova2321@gmail.com>
2021-09-02 01:14:06 +0300

    Created basic interfaces for scrapers

---
+++ src/Application/Contracts/ISearchScraper.cs
@@ -0,0 +1,10 @@
+﻿using Aurora.Application.Models;
+using System.Threading.Tasks;
+
+namespace Aurora.Application.Contracts
+{
+    public interface ISearchScraper
+    {
+        Task<SearchResult> Search(SearchRequest request);
+    }
+}
