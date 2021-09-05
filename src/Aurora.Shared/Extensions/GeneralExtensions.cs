4615fdbbea6b9b25af3e472021e58342db8eccb4
korov <korova2321@gmail.com>
2021-09-05 22:45:56 +0300

    Created project for common utilities

---
+++ src/Aurora.Shared/Extensions/GeneralExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aurora.Shared.Extensions
+{
+    internal static class GeneralExtensions
+    {
+        public static bool IsEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNotEmpty(this string str)
+        {
+            return !str.IsEmpty();
+        }
+
+        public static bool IsNotNull(this object obj)
+        {
+            return !(obj is null);
+        }
+
+        public static bool None<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return !collection.Any(predicate);
+        }
+
+        public static TProperty With<T, TProperty>(this T self, Func<T, TProperty> propertyFunction,
+            TProperty defaultValue = default) where T : class
+        {
+            TProperty result;
+            if (self == default(T))
+            {
+                result = defaultValue;
+            }
+            else
+            {
+                result = propertyFunction(self);
+            }
+            return result;
+        }
+
+        public static bool NotEndsWith(this string str, string endStr)
+        {
+            return !str.EndsWith(endStr);
+        }
+
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> source)
+        {
+            return source.SelectMany(x => x);
+        }
+    }
+}
---
+++ src/Aurora.Shared/Models/SuccessOrFailure.cs
@@ -0,0 +1,36 @@
+﻿namespace Aurora.Shared.Models
+{
+    public struct SuccessOrFailure
+    {
+        public bool IsSuccessfull { get; set; }
+        public bool IsFailure => !IsSuccessfull;
+
+        public string FailureMessage { get; set; }
+
+        public static SuccessOrFailure CreateSuccess()
+        {
+            return new SuccessOrFailure
+            {
+                IsSuccessfull = true
+            };
+        }
+
+        public static SuccessOrFailure CreateFailure(string failureMessage = null)
+        {
+            return new SuccessOrFailure
+            {
+                IsSuccessfull = false,
+                FailureMessage = failureMessage
+            };
+        }
+
+        public static implicit operator SuccessOrFailure(bool flag)
+        {
+            if (flag == false)
+            {
+                return CreateFailure();
+            }
+            return CreateSuccess();
+        }
+    }
+}
