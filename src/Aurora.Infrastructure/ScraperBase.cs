43585b70133ef2660e8c83c855ce85cd81b2bc69
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-09-08 12:41:03 +0300

    add scrapper base type

---
+++ src/Aurora.Infrastructure/ScraperBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Aurora.Application.Contracts;
+using Aurora.Application.Models;
+using Aurora.Shared.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Aurora.Infrastructure
+{
+    public enum ScraperStatusCode
+    {
+        Success = 0,
+        HandledError = 1,
+        UnhandledError = 2
+    }
+
+    public abstract class ScraperBase : ISearchScraper
+    {
+        private const string LogFormat = "StatusCode: '{code}', scraper '{scraperName}' {scraperMessage} in {time}";
+
+        private readonly ILogger _logger;
+
+        public ScraperBase(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ValueOrNull<SearchResult>> Search(SearchRequest request, CancellationToken token = default)
+        {
+            Stopwatch watch = new();
+            watch.Start();
+
+            ValueOrNull<SearchResult> result;
+            ScraperStatusCode code;
+            try
+            {
+                result = await SearchInner(request, token);
+                if (result.HasValue)
+                {
+                    code = ScraperStatusCode.Success;
+                }
+                else
+                {
+                    code = ScraperStatusCode.HandledError;
+                }
+               
+            }
+            catch
+            {
+                code = ScraperStatusCode.UnhandledError;
+                result = ValueOrNull<SearchResult>.CreateNull("Unahndled exception");
+            }
+
+            watch.Stop();
+
+            string time = watch.ElapsedMilliseconds.ToString();
+            string scraperName = this.GetType().Name;
+            string codeMessage = code.ToString();
+
+            _logger.LogDebug(LogFormat, codeMessage, scraperName, codeMessage, time);
+
+            return result;
+        }
+
+        public abstract Task<ValueOrNull<SearchResult>> SearchInner(SearchRequest request, CancellationToken token = default);
+    }
+}
