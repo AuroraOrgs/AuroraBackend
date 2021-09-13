25f95272025bb18f2c6effcd063b045f6ac02661
korov <korova2321@gmail.com>
2021-09-14 02:09:26 +0300

    Added selenium

--- src/Aurora.Infrastructure/DIExtensions.cs
+++ src/Aurora.Infrastructure/DIExtensions.cs
@@ -1,19 +1,23 @@
 ﻿using Aurora.Application.Contracts;
+using Aurora.Infrastructure.Config;
 using Aurora.Infrastructure.Contracts;
 using Aurora.Infrastructure.Scrapers;
 using Aurora.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Aurora.Infrastructure
 {
     public static class DIExtensions
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
         {
             services.AddSingleton<ISearchScraperCollector, SearchScraperCollector>();
             services.AddSingleton<PornhubScraper>();
 
             services.AddSingleton<IWebClientService, WebClientService>();
+
+            services.Configure<SeleniumConfig>(option => config.GetSection("Selenium").Bind(option));
 
             return services;
         }
---
+++ src/Aurora.Infrastructure/Services/DriverInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Aurora.Infrastructure.Config;
+using Microsoft.Extensions.Options;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aurora.Infrastructure.Services
+{
+    public class DriverInitializer
+    {
+        private readonly SeleniumConfig _options;
+
+        public DriverInitializer(IOptions<SeleniumConfig> options)
+        {
+            _options = options.Value;
+        }
+
+        public Task<RemoteWebDriver> Initialize()
+        {
+            string seleniumLocation = _options.SeleniumLocation;
+            Uri uri = new Uri(seleniumLocation);
+            var options = new FirefoxOptions();
+            RemoteWebDriver driver = new RemoteWebDriver(uri, options.ToCapabilities());
+            return Task.FromResult(driver);
+        }
+    }
+}
