b5352ff44819c894a7e69579cbfd0d7581147855
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-10-11 21:33:49 +0300

    ruslan: Fix video scraping, add image scraping

--- src/Aurora.Infrastructure/Scrapers/XvideosScraper.cs
+++ src/Aurora.Infrastructure/Scrapers/XvideosScraper.cs
@@ -87,7 +87,7 @@ namespace Aurora.Infrastructure.Scrapers
                 // e.g: https://www.xvideos.com/?k=test+value&p=1
                 var searchTermUrlFormatted = FormatTermToUrl(searchTerm);
                 var searchPageUrl = $"{_baseUrl}/?k={searchTermUrlFormatted}&p={pageNumber}";
-                await _clientProvider.SetUserString(client);
+                await _clientProvider.SetTls12UserString(client);
                 var htmlSearchPage = client.DownloadString(searchPageUrl);
                 htmlDocument.LoadHtml(htmlSearchPage);
 
@@ -114,16 +114,17 @@ namespace Aurora.Infrastructure.Scrapers
                     if (currentLinkImageNode != null)
                     {
                         var currentLinkImageAttributes = currentLinkImageNode.Attributes;
-                        searchVideoItem.ImagePreviewUrl = currentLinkImageAttributes["data-thumb_url"].Value;
+                        searchVideoItem.ImagePreviewUrl = currentLinkImageAttributes["data-src"]?.Value;
                     }
 
                     var currentLinkAttributes = videoLinkNode.Attributes;
-                    var videoLink = currentLinkAttributes["href"].Value;
-                    if (videoLink.Contains("video"))
+                    var videoLink = currentLinkAttributes["href"]?.Value;
+                    if (videoLink is not null && videoLink.Contains("video") && !videoLink.Contains("videos"))
                         searchVideoItem.SearchItemUrl = $"{_baseUrl}{videoLink}";
 
                     urlsCount++;
-                    videoItems.Add(searchVideoItem);
+                    if (searchVideoItem.SearchItemUrl is not null && searchVideoItem.ImagePreviewUrl is not null)
+                        videoItems.Add(searchVideoItem);
                 }
 
                 pageNumber++;
@@ -139,10 +140,71 @@ namespace Aurora.Infrastructure.Scrapers
             throw new NotImplementedException();
         }
 
-        private async Task<IEnumerable<SearchItem>> ScrapImages(string requestSearchTerm,
-            int requestResponseItemsMaxCount)
+        private async Task<IEnumerable<SearchItem>> ScrapImages(string searchTerm,
+            int maxNumberOfImageUrls)
         {
-            throw new NotImplementedException();
+            List<SearchItem> imageItems = new();
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument
+            {
+                OptionFixNestedTags = true
+            };
+
+            var pageNumber = 1;
+
+            var urlsCount = 0;
+
+            using var client = await _clientProvider.Provide();
+            for (var i = 0; i < PAGE_NUMBER_LIMIT; i++)
+            {
+                if (urlsCount >= maxNumberOfImageUrls)
+                {
+                    break;
+                }
+
+                // e.g: https://www.xvideos.com/?k=test+value&p=1
+                var searchTermUrlFormatted = FormatTermToUrl(searchTerm);
+                var searchPageUrl = $"{_baseUrl}/?k={searchTermUrlFormatted}&p={pageNumber}";
+                await _clientProvider.SetTls12UserString(client);
+                var htmlSearchPage = client.DownloadString(searchPageUrl);
+                htmlDocument.LoadHtml(htmlSearchPage);
+
+                var bodyNode = htmlDocument.DocumentNode
+                    ?.SelectSingleNode("//body");
+                var videoLinksNodes = bodyNode
+                    ?.SelectNodes("//a");
+
+                if (videoLinksNodes is null)
+                {
+                    break;
+                }
+
+                foreach (var videoLinkNode in videoLinksNodes)
+                {
+                    SearchItem searchImageItem = new()
+                    {
+                        Option = SearchOption.Image
+                    };
+
+                    var currentLinkImageNode = videoLinkNode.ChildNodes
+                        .FirstOrDefault(n => n.Name == "img");
+
+                    if (currentLinkImageNode != null)
+                    {
+                        var currentLinkImageAttributes = currentLinkImageNode.Attributes;
+                        searchImageItem.SearchItemUrl = currentLinkImageAttributes["data-src"]?.Value;
+                    }
+
+                    urlsCount++;
+                    if (searchImageItem.SearchItemUrl is not null)
+                        imageItems.Add(searchImageItem);
+                }
+
+                pageNumber++;
+                await Task.Delay(1000);
+            }
+
+            return imageItems;
         }
 
         private static string FormatTermToUrl(string term)
