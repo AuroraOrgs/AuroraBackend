0edced7fb0b026dfdc406bf00f7701569584f141
korov <korova2321@gmail.com>
2021-12-27 13:07:24 +0200

    Create migration

---
+++ src/Aurora.Infrastructure/Migrations/20211227110530_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aurora.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SearchTerm = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Website = table.Column<int>(type: "integer", nullable: false),
+                    ContentOption = table.Column<int>(type: "integer", nullable: false),
+                    OccurredCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Result",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ImagePreviewUrl = table.Column<string>(type: "text", nullable: true),
+                    SearchItemUrl = table.Column<string>(type: "text", nullable: true),
+                    RequestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Result", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Result_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Result_RequestId",
+                table: "Result",
+                column: "RequestId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Result");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+        }
+    }
+}
