c6233eb7e06bdcd898269c464327423b605173d1
korov <korova2321@gmail.com>
2022-01-07 12:38:24 +0200

    Add serilog logging to db

--- src/Aurora.Presentation/Program.cs
+++ src/Aurora.Presentation/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace Aurora.Presentation
 {
@@ -7,8 +9,37 @@ namespace Aurora.Presentation
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            host.Run();
+            try
+            {
+                using IHost host = CreateHostBuilder(args).Build();
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                if (Log.Logger == null || Log.Logger.GetType().Name == "SilentLogger")
+                {
+                    // Loading configuration or Serilog failed.
+                    // This will create a logger that can be captured by Azure logger.
+                    // To enable Azure logger, in Azure Portal:
+                    // 1. Go to WebApp
+                    // 2. App Service logs
+                    // 3. Enable "Application Logging (Filesystem)", "Application Logging (Filesystem)" and "Detailed error messages"
+                    // 4. Set Retention Period (Days) to 10 or similar value
+                    // 5. Save settings
+                    // 6. Under Overview, restart web app
+                    // 7. Go to Log Stream and observe the logs
+                    Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .WriteTo.Console()
+                        .CreateLogger();
+                }
+
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -16,7 +47,16 @@ namespace Aurora.Presentation
             var builder = Host.CreateDefaultBuilder(args);
             return builder.ConfigureWebHostDefaults(webBuilder =>
                {
-                   webBuilder.UseStartup<Startup>();
+                   webBuilder
+                    .UseStartup<Startup>()
+                    .UseSerilog((hostingContext, loggerConfiguration) =>
+                    {
+                        loggerConfiguration
+                            .ReadFrom.Configuration(hostingContext.Configuration)
+                            .Enrich.FromLogContext()
+                            .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                            .Enrich.WithProperty("Environment", hostingContext.HostingEnvironment);
+                    });
                });
         }
     }
