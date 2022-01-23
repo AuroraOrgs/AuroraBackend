56c5fc95ef3e451ccc8cca7459e4421d0ac0f251
korov <korova2321@gmail.com>
2022-01-23 20:31:31 +0200

    Improve debugging

--- src/Aurora.Infrastructure/SearchContext.cs
+++ src/Aurora.Infrastructure/SearchContext.cs
@@ -1,18 +1,20 @@
 ﻿using Aurora.Application.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Aurora.Infrastructure;
-
-public class SearchContext : DbContext
+namespace Aurora.Infrastructure
 {
-    public SearchContext(DbContextOptions<SearchContext> options) : base(options) { }
 
-    public DbSet<SearchRequest> Request { get; set; }
-    public DbSet<SearchResult> Result { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class SearchContext : DbContext
     {
-        modelBuilder.Entity<SearchRequest>()
-           .HasIndex(p => new { p.SearchTerm, p.Website, p.ContentOption }).IsUnique();
+        public SearchContext(DbContextOptions<SearchContext> options) : base(options) { }
+
+        public DbSet<SearchRequest> Request { get; set; }
+        public DbSet<SearchResult> Result { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SearchRequest>()
+               .HasIndex(p => new { p.SearchTerm, p.Website, p.ContentOption }).IsUnique();
+        }
     }
 }
