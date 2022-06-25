2bf97940ce5fef3c5cd3b6439fe1d881e0777f3f
korov <korova2321@gmail.com>
2022-06-25 14:42:40 +0300

    version 123

--- scripts/Docker/release.cmd
+++ scripts/Docker/release.cmd
@@ -1,25 +1,26 @@
 cd ..
 cd ..
 cd /src
-# docker hub username
 set USERNAME=yaroslavholota
-# image name
 set IMAGE=aurora
-# ensure we're up to date
 git pull
-# bump version
-docker run --rm -v "$PWD":/app treeder/bump patch
-version=`cat VERSION`
-echo "version: $version"
-# run build
-./build.sh
-# tag it
+::Load current location
+for /f %%A in ('cd') do set "pwd=%%A"
+::Bump version
+docker run --rm -v "%PWD%":/app treeder/bump patch
+::Get version
+for /f %%B in ('type VERSION') do set "version=%%B"
+echo version: %version%
+::Build container
+../scripts/Docker/build.cmd
+::Commit
 git add -A
-git commit -m "version $version"
-git tag -a "$version" -m "version $version"
+git commit -m "version %version%"
+git tag -a "%version%" -m "version %version%"
 git push
 git push --tags
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-# push it
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+::Tag
+docker tag %USERNAME%/%IMAGE%:latest %USERNAME%/%IMAGE%:%version%
+::Push
+docker push %USERNAME%/%IMAGE%:latest
+docker push %USERNAME%/%IMAGE%:%version%
---
+++ scripts/Docker/test.cmd
@@ -0,0 +1,1 @@
+for /f %%A in ('cd') do set "pwd=%%A"
