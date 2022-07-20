4b9a8eec1b465f0421ab3a3a6ca04dbcdd97007a
korov <korova2321@gmail.com>
2022-07-20 11:41:46 +0300

    version 0.0.1

--- scripts/Docker/build.cmd
+++ scripts/Docker/build.cmd
@@ -4,5 +4,5 @@ cd ..
 cd ..
 cd src
 docker build -t %USERNAME%/%IMAGE%:latest -f Dockerfile .
-docker build -t %USERNAME%/%IMAGE%:latest --platform linux/arm64/v8 -f Dockerfile-arm64 .
+docker build -t %USERNAME%/%IMAGE%-arm64:latest --platform linux/arm64/v8 -f Dockerfile-arm64 .
 cd ..
