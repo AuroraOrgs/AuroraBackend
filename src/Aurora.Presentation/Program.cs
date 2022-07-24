d91069870dbd85984a0b096205a3a8d836303b7f
korov <korova2321@gmail.com>
2022-07-24 17:10:24 +0300

    Remove azure comment

--- src/Aurora.Presentation/Program.cs
+++ src/Aurora.Presentation/Program.cs
@@ -26,16 +26,6 @@ namespace Aurora.Presentation
             {
                 if (Log.Logger == null || Log.Logger.GetType().Name == "SilentLogger")
                 {
-                    // Loading configuration or Serilog failed.
-                    // This will create a logger that can be captured by Azure logger.
-                    // To enable Azure logger, in Azure Portal:
-                    // 1. Go to WebApp
-                    // 2. App Service logs
-                    // 3. Enable "Application Logging (Filesystem)", "Application Logging (Filesystem)" and "Detailed error messages"
-                    // 4. Set Retention Period (Days) to 10 or similar value
-                    // 5. Save settings
-                    // 6. Under Overview, restart web app
-                    // 7. Go to Log Stream and observe the logs
                     Log.Logger = new LoggerConfiguration()
                         .MinimumLevel.Debug()
                         .WriteTo.Console()
