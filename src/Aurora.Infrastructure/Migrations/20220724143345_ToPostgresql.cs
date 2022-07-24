fddf06a13a4307bcc3b511e0b58a022b10d117ef
korov <korova2321@gmail.com>
2022-07-24 18:07:52 +0300

    Add migration

---
+++ src/Aurora.Infrastructure/Migrations/20220724143345_ToPostgresql.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aurora.Infrastructure.Migrations
+{
+    public partial class ToPostgresql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SearchTerm = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Website = table.Column<int>(type: "integer", nullable: false),
+                    ContentOption = table.Column<int>(type: "integer", nullable: false),
+                    OccurredCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Queue",
+                columns: table => new
+                {
+                    QueueId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SearchRequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    QueuedTimeUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsProcessed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Queue", x => x.QueueId);
+                    table.ForeignKey(
+                        name: "FK_Queue_Request_SearchRequestId",
+                        column: x => x.SearchRequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Result",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ImagePreviewUrl = table.Column<string>(type: "text", nullable: true),
+                    SearchItemUrl = table.Column<string>(type: "text", nullable: true),
+                    FoundTimeUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    RequestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Result", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Result_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Queue_SearchRequestId",
+                table: "Queue",
+                column: "SearchRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Request_SearchTerm_Website_ContentOption",
+                table: "Request",
+                columns: new[] { "SearchTerm", "Website", "ContentOption" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Result_RequestId",
+                table: "Result",
+                column: "RequestId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Queue");
+
+            migrationBuilder.DropTable(
+                name: "Result");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+        }
+    }
+}
