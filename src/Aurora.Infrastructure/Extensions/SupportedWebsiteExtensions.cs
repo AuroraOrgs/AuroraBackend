da6e4d37c568d6c0df08b26a2ee22c1246f1e7ab
korov <korova2321@gmail.com>
2022-07-31 11:02:19 +0300

    Introduce footfetishbooru scraper

--- src/Aurora.Infrastructure/Extensions/SupportedWebsiteExtensions.cs
+++ src/Aurora.Infrastructure/Extensions/SupportedWebsiteExtensions.cs
@@ -8,8 +8,9 @@ namespace Aurora.Infrastructure.Extensions
         public static string GetBaseUrl(this SupportedWebsite website) =>
             website switch
             {
-                SupportedWebsite.Xvideos => "https://www.xvideos.com",
+                SupportedWebsite.XVideos => "https://www.xvideos.com",
                 SupportedWebsite.Pornhub => "https://www.pornhub.com",
+                SupportedWebsite.FootFetishBooru => "https://footfetishbooru.booru.org",
                 _ => throw new Exception($"Base url for {website} is not configured"),
             };
     }
