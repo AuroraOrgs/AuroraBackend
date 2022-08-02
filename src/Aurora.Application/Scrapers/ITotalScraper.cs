6662f0a5c22463b509b9028d34ac2c3224ad5e95
korov <korova2321@gmail.com>
2022-08-02 12:23:08 +0300

    Introduce total scraper interface

---
+++ src/Aurora.Application/Scrapers/ITotalScraper.cs
@@ -0,0 +1,13 @@
+﻿using Aurora.Application.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Aurora.Application.Scrapers
+{
+    public interface ITotalScraper
+    {
+        SupportedWebsite Website { get; }
+        IEnumerable<SearchOption> Options { get; }
+        Task<(string Term, List<SearchItem> Items)> Scrap();
+    }
+}
