6549dcac5f0e2e0160dc6693fe662b71ef27d0b2
korov <korova2321@gmail.com>
2022-08-04 02:20:28 +0300

    Add sample scraper

---
+++ src/Aurora.Infrastructure/Scrapers/FootFetishBooruTotalScraper.cs
@@ -0,0 +1,23 @@
+﻿using Aurora.Application.Models;
+using Aurora.Application.Scrapers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Aurora.Infrastructure.Scrapers
+{
+    public class FootFetishBooruTotalScraper : ITotalScraper
+    {
+        public SupportedWebsite Website => SupportedWebsite.FootFetishBooru;
+
+        public async Task<IEnumerable<(string Term, List<SearchItem> Items)>> Scrap()
+        {
+            List<(string Term, List<SearchItem> Items)> result = new();
+            result.Add(("Test", new List<SearchItem>()
+            {
+                new SearchItem(SearchOption.Video, "abcs", "dsa"),
+                new SearchItem(SearchOption.Video, "dsads", "asasa")
+            }));
+            return result;
+        }
+    }
+}
