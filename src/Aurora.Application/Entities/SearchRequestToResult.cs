610f36836c9bb0fcd5434cdcd373e1db74a4b946
korov <korova2321@gmail.com>
2022-08-07 09:51:05 +0300

    Change relation between request and result

---
+++ src/Aurora.Application/Entities/SearchRequestToResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora.Application.Entities
+{
+    public class SearchRequestToResult
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid SearchRequestId { get; set; }
+        public Guid SearchResultId { get; set; }
+
+        public SearchRequest SearchRequest { get; set; } = null!;
+        public SearchResult SearchResult { get; set; } = null!;
+    }
+}
--- src/Aurora.Infrastructure/SearchContext.cs
+++ src/Aurora.Infrastructure/SearchContext.cs
@@ -10,11 +10,15 @@ namespace Aurora.Infrastructure
         public DbSet<SearchRequest> Request { get; set; }
         public DbSet<SearchResult> Result { get; set; }
         public DbSet<SearchRequestQueueItem> Queue { get; set; }
+        public DbSet<SearchRequestToResult> RequestToResult { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SearchRequest>()
                .HasIndex(p => new { p.SearchTerm, p.Website, p.ContentType }).IsUnique();
+
+            modelBuilder.Entity<SearchRequestToResult>()
+                .HasIndex(p => new { p.SearchResultId, p.SearchRequestId }).IsUnique();
         }
     }
 }
