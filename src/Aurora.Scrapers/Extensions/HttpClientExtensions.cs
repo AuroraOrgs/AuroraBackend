a373cac8fbbcb7773526c053c47fead1eba6bbc2
korov <korova2321@gmail.com>
2022-08-25 12:55:31 +0300

    Remove unused httpclient extension

--- src/Aurora.Scrapers/Extensions/HttpClientExtensions.cs
+++ src/Aurora.Scrapers/Extensions/HttpClientExtensions.cs
@@ -2,23 +2,6 @@
 
 public static class HttpClientExtensions
 {
-    public static async Task<bool> TryLoadDocumentFromUrl(this HttpClient client, HtmlDocument htmlDocument, string url)
-    {
-        bool succeeded;
-        var response = await client.GetAsync(url);
-        if (response.IsSuccessStatusCode)
-        {
-            var htmlContent = await response.Content.ReadAsStringAsync();
-            htmlDocument.LoadHtml(htmlContent);
-            succeeded = true;
-        }
-        else
-        {
-            succeeded = false;
-        }
-        return succeeded;
-    }
-
     public static async Task<ValueOrNull<HtmlDocument>> TryLoadDocumentFromUrl(this HttpClient client, string url)
     {
         var response = await client.GetAsync(url);
