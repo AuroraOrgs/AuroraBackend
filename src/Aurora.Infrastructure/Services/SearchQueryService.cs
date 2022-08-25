af27e4bed06e55c858cb01336fa3d53b5490f36d
korov <korova2321@gmail.com>
2022-08-25 22:16:02 +0300

    Add no tracking to load method

--- src/Aurora.Infrastructure/Services/SearchQueryService.cs
+++ src/Aurora.Infrastructure/Services/SearchQueryService.cs
@@ -24,6 +24,7 @@ public class SearchQueryService : ISearchQueryService
         if (idsToLoad.Any())
         {
             var filteredResults = _context.Result
+                .AsNoTracking()
                 .Include(x => x.SearchRequest)
                 .Where(request => idsToLoad.Contains(request.SearchRequestId));
             var query = filteredResults;
