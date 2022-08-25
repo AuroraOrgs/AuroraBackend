c97a91e64cfd322f688f0025c40db4eeff292676
korov <korova2321@gmail.com>
2022-08-25 12:48:14 +0300

    Add scrapers testing project

--- src/Aurora.Scrapers/Extensions/StringExtensions.cs
+++ src/Aurora.Scrapers/Extensions/StringExtensions.cs
@@ -2,11 +2,11 @@
 
 namespace Aurora.Scrapers.Extensions;
 
-internal static class StringExtensions
+public static class StringExtensions
 {
     public static string FormatTermToUrl(this string term) => term.Replace(" ", "+");
 
-    private static Regex _numbersRegex = new Regex("\\d+(\\.\\d+)?", RegexOptions.Multiline | RegexOptions.Compiled);
+    private static readonly Regex _numbersRegex = new("\\d+(\\.\\d+)?", RegexOptions.Multiline | RegexOptions.Compiled);
     public static List<long> ExtractAllNumbers(this string str)
     {
         List<long> results = new();
