1b6bd0efb56dae1f6b8228905eabd3c14c3e8c07
korov <korova2321@gmail.com>
2022-09-02 00:23:46 +0300

    Improve docs

---
+++ src/Aurora.Application/Models/SearchRequestOptionStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace Aurora.Application.Models;
+
+public enum SearchRequestOptionStatus
+{
+    /// <summary>
+    /// Search request is being processed
+    /// </summary>
+    Queued,
+    /// <summary>
+    /// Search request is not being processed and has never been
+    /// </summary>
+    NotFetched,
+    /// <summary>
+    /// Search request is processed
+    /// </summary>
+    Fetched
+}
