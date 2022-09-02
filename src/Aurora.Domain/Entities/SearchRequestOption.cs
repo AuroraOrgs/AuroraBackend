ba3a206c4d0927acbad89a07c59cd6e5b96115fb
korov <korova2321@gmail.com>
2022-09-03 02:01:59 +0300

    Fix warnings

--- src/Aurora.Domain/Entities/SearchRequestOption.cs
+++ src/Aurora.Domain/Entities/SearchRequestOption.cs
@@ -10,8 +10,8 @@ public class SearchRequestOption
     public Guid Id { get; set; }
 
     public SearchOptionTerm SearchTerm { get; set; } = null!;
-    public EnumWrapper<SupportedWebsite> Website { get; set; }
-    public EnumWrapper<ContentType> ContentType { get; set; }
+    public EnumWrapper<SupportedWebsite> Website { get; set; } = null!;
+    public EnumWrapper<ContentType> ContentType { get; set; } = null!;
 
     public int OccurredCount { get; set; }
     public IEnumerable<SearchOptionSnapshot> Snapshots { get; set; } = null!;
