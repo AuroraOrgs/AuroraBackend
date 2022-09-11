5ad4611543b988aaaa8815a146f0cc219d80d0c6
korov <korova2321@gmail.com>
2022-09-11 15:56:22 +0300

    Add get snapshots query

---
+++ src/Aurora.Application/Models/GetSnapshostResult.cs
@@ -0,0 +1,6 @@
+﻿using Aurora.Shared.Models;
+using System.Collections.Immutable;
+
+namespace Aurora.Application.Models;
+
+public record GetSnapshostResult(ImmutableList<SearchResultDto> Results);
