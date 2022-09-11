761f57ff527d2788e9b86e61bf097bf6ee3baf5e
korov <korova2321@gmail.com>
2022-09-11 16:08:09 +0300

    Move paging defaults into paging class

--- src/Aurora.Application/Commands/SearchCommand.cs
+++ src/Aurora.Application/Commands/SearchCommand.cs
@@ -15,25 +15,7 @@ public class SearchCommand : IRequest<SearchCommandResult>
     public SearchCommand(SearchRequestDto searchRequest, int? pageNumber, int? pageSize, string? userId)
     {
         SearchRequest = searchRequest;
-        if (pageNumber is null && pageSize is null)
-        {
-            Paging = null;
-        }
-        else
-        {
-            if (pageNumber is null)
-            {
-                pageNumber = 0;
-            }
-            else
-            {
-                if (pageSize is null)
-                {
-                    pageSize = 25;
-                }
-            }
-            Paging = new PagingOptions(pageNumber.Value!, pageSize!.Value);
-        }
+        Paging = PagingOptions.Create(pageNumber, pageSize);
         UserId = userId;
     }
 
