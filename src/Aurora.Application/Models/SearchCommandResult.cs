9cef58b8585d80f63256fb849318654acc0d600a
korov <korova2321@gmail.com>
2022-09-11 08:16:27 +0300

    Remove completed todo

--- src/Aurora.Application/Models/SearchCommandResult.cs
+++ src/Aurora.Application/Models/SearchCommandResult.cs
@@ -3,5 +3,4 @@
 namespace Aurora.Application.Models;
 
 public record SearchCommandResult(List<OneOf<SearchResultDto, QueuedResult>> Results, long ItemsCount);
-//TODO: Add date of queue
 public record QueuedResult(SupportedWebsite Website, DateTime QueuedTime);
