72d231b9046b4f31a616768b925e455df3b3be9f
korov <korova2321@gmail.com>
2022-10-18 13:39:20 +0300

    Add new config

---
+++ src/Aurora.Application/Config/RunnerConfig.cs
@@ -0,0 +1,11 @@
+﻿using Aurora.Shared.Config;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aurora.Application.Config;
+
+[ConfigSection("Scrapers", "Runner")]
+public class RunnerConfig
+{
+    [Range(1, Int32.MaxValue)]
+    public int MaxConcurrentScrapers { get; set; } = 5;
+}
